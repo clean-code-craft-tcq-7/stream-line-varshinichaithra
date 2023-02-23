de493764ef10ae81863cb2e969fa2709d2d7b789
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-23 17:30:44 +0530

    Update Receiver.c

--- Receiver/Receiver.c
+++ Receiver/Receiver.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  char *lineptr = NULL;
+  size_t len;
+  size_t read;
+  //Read summary
+  if((read = getline(&lineptr, &len, stdin)) != -1){
+     printf("\n%s", lineptr);
+  }
+  //
   printf("\nTest receiver workflow");
   return 0;
 }
