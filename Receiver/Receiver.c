50b7944d3a4571dd859c3aaba564aa233ef81312
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-24 17:45:31 +0530

    Update Receiver.c
    Test

--- Receiver/Receiver.c
+++ Receiver/Receiver.c
@@ -2,16 +2,31 @@
 #include <string.h>
 #include "test-sample.h"
 
-int main(int argc, char* argv[]) {
-  
+int main(int argc, char* argv[]) {  
   if(argc < 2){
     printf("\n./* [option]");
     return 0;
   }
   if(strncmp(argv[1], "test", strlen("test")) == 0){
-     TestSensorValue();
-     /* TestMinMax();
-      TestMovingAverage();*/
+      FILE* fp;
+      fp = fopen("sample1.txt", "w+");
+      if(fp == NULL){
+         printf("\n file open failure");
+      }
+     else{
+        printf("\n file open success");
+      }
+    
+      fp = fopen("sample.txt", "r");
+      if(fp == NULL){
+         printf("\n file open failure");
+      }
+     else{
+        printf("\n file open success");
+      }
+      TestSensorValue();
+      TestMinMax();
+      TestMovingAverage();
       printf("\n Test receiver workflow");
   } 
   return 0;
