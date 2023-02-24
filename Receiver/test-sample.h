d3b979bbebcbe8264200817c27ec56a5a7e24c23
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-24 17:56:22 +0530

    Update test-sample.h

--- Receiver/test-sample.h
+++ Receiver/test-sample.h
@@ -1,7 +1,8 @@
 #ifndef TEST_SAMPLE_H_
 #define TEST_SAMPLE_H_
+#include <stdio.h>
 
-void TestSensorValue(void);
+void TestSensorValue(FILE*);
 void TestMinMax(void);
 void TestMovingAverage(void);
 
