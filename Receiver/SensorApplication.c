3945449e9859653194f65296a88c799ffe35be15
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-24 14:34:48 +0530

    Create SensorApplication.c

---
+++ Receiver/SensorApplication.c
@@ -0,0 +1,29 @@
+
+#include "SensorApplication.h"
+
+int GetMin(float a, float b){
+    return (a < b);
+}
+
+int GetMax(float a, float b){
+    return (a > b);
+}
+
+//Get Min and max value
+float GetMinMax(float list[], int size, int (*input)(float, float)){
+    int i;
+    int num = list[0];
+    for(i = 1; i < size; i++){
+        if(input(num, list[i]) == 0){
+            num = list[i];
+        }
+    }
+    return num;
+}
+
+float GetmovingAverage(float *list, float *sum, int pos, int len, float nextNum)
+{
+   *sum = *sum - list[pos] + nextNum;
+   list[pos] = nextNum;
+   return *ptrSum / len;
+}
